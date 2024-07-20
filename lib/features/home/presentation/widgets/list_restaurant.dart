4fe966c2bb86cfdc87989c1d3526a8325bd4cf01
Xiraj <akbarsirajuddinh@gmail.com>
2024-07-20 19:25:21 +0700

    sorting recomdation restaurant

--- lib/features/home/presentation/widgets/list_restaurant.dart
+++ lib/features/home/presentation/widgets/list_restaurant.dart
@@ -16,6 +16,11 @@ class ListRestaurant extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
+    // Filter out null reviews and create a new sorted list
+    final sortedMenuItems = List<GetRestaurantResponse>.from(menuItems)
+      ..removeWhere((item) => item.review == null)
+      ..sort((a, b) => b.review!.compareTo(a.review!));
+
     return BlocProvider<GetRestaurantBloc>(
       create: (context) => GetRestaurantBloc()
         ..add(
@@ -51,20 +56,23 @@ class ListRestaurant extends StatelessWidget {
                   );
                 } else if (state is RestaurantLoadedState) {
                   return ListView.separated(
-                    itemCount: isHome ? 5 : menuItems.length,
+                    itemCount: isHome ? 5 : sortedMenuItems.length,
                     itemBuilder: (context, index) {
-                      final menuItem = menuItems[index];
+                      final menuItem = sortedMenuItems[index];
                       return RecomendationResto(
                         name: menuItem.namaToko!,
                         address: menuItem.alamat!,
                         imageUrl: menuItem.gambarToko!,
-                        rating: menuItem.review! / 10,
+                        rating:
+                            menuItem.review != null ? menuItem.review! / 10 : 0,
                         onPressed: () {
-                          Navigator.pushNamed(
-                            context,
-                            '/information-restaurant',
-                            arguments: menuItem.id,
-                          );
+                          if (menuItem.id != null) {
+                            Navigator.pushNamed(
+                              context,
+                              '/information-restaurant',
+                              arguments: menuItem.id,
+                            );
+                          }
                         },
                       );
                     },
